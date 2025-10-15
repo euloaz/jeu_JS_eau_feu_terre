65b36b916278d4bcaf401f29ee1e2d54ea6b1aa8
Héloïse <heloise.marie-oem@outlook.fr>
2025-10-15 14:29:52 +0200

    ajout affichage

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,43 @@ const feu = document.querySelector('#feu');
 const eau = document.querySelector('#eau');
 const terre = document.querySelector('#terre');
 
+const computerScore = document.querySelector('.computer-side .content .score span');
+const userScore = document.querySelector('.player-side .content .score span');
+
+const userResult = document.querySelector('.player-side .content .result');
+const computerResult = document.querySelector('.computer-side .content .result');
+const message = document.querySelector('.message');
+
+let uScore = 0;
+let pcScore = 0;
 function userChoice(choice){
     return choice;
 }
 feu.addEventListener("click", () => {
-    playGame(userChoice('Feu'),computerChoice())
+    let player = userChoice('Feu');
+    let pc = computerChoice();
+
+    userResult.textContent = player;
+    computerResult.textContent = pc;
+    message.textContent = playGame(player,pc);
+    userScore.textContent = uScore;
+    computerScore.textContent = pcScore;
 })
 eau.addEventListener("click", () =>{
-   playGame(userChoice('Eau'),computerChoice())
+    let player = userChoice('Eau');
+    let pc = computerChoice();
+
+    userResult.textContent = player;
+    computerResult.textContent = pc;
+    message.textContent = playGame(player,pc);
 })
 terre.addEventListener("click", () =>{
-    playGame(userChoice('Terre'),computerChoice())
+    let player = userChoice('Terre');
+    let pc = computerChoice();
+
+    userResult.textContent = player;
+    computerResult.textContent = pc;
+    message.textContent = playGame(player,pc);
 })
 
 function playGame(userChoice, computerChoice){
@@ -26,17 +52,20 @@ function playGame(userChoice, computerChoice){
         return 'Egalité';
     }
     else if (userChoice === "Feu" && computerChoice === "Terre"){
+        uScore++;
         return 'Bravo ! Tu as gagné!';
     }
     else if (userChoice === "Eau" && computerChoice === "Feu"){
+        uScore++;
         return 'Bravo ! Tu as gagné!';
     }
     else if (userChoice === "Terre" && computerChoice === "Eau"){
+        uScore++;
          return 'Bravo ! Tu as gagné!';
     }
     else{
+        pcScore++;
         return 'PERDU BOLOSS';
     }
-
 }
 
