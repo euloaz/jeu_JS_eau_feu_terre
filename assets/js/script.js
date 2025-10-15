2273fab2d739a56d1a771ab3fb5c831fb717e03b
Héloïse <heloise.marie-oem@outlook.fr>
2025-10-15 16:02:44 +0200

    ajout style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const choices = ["Feu","Eau","Terre"]
+const choices = ["\u{1F525}","\u{1F4A7}","\u{1F331}"]
 function computerChoice(){
     let random = Math.floor(Math.random()* choices.length);
     return choices[random];
@@ -8,11 +8,11 @@ const feu = document.querySelector('#feu');
 const eau = document.querySelector('#eau');
 const terre = document.querySelector('#terre');
 
-const computerScore = document.querySelector('.computer-side .content .score span');
-const userScore = document.querySelector('.player-side .content .score span');
+const computerScore = document.querySelector('.arena .computer-side .score');
+const userScore = document.querySelector('.arena .player-side  .score');
 
-const userResult = document.querySelector('.player-side .content .result');
-const computerResult = document.querySelector('.computer-side .content .result');
+const userResult = document.querySelector('.player-side .result');
+const computerResult = document.querySelector('.computer-side .result');
 const message = document.querySelector('.message');
 
 let uScore = 0;
@@ -21,7 +21,7 @@ function userChoice(choice){
     return choice;
 }
 feu.addEventListener("click", () => {
-    let player = userChoice('Feu');
+    let player = userChoice("\u{1F525}");
     let pc = computerChoice();
 
     userResult.textContent = player;
@@ -31,7 +31,7 @@ feu.addEventListener("click", () => {
     computerScore.textContent = pcScore;
 })
 eau.addEventListener("click", () =>{
-    let player = userChoice('Eau');
+    let player = userChoice("\u{1F4A7}");
     let pc = computerChoice();
 
     userResult.textContent = player;
@@ -39,27 +39,26 @@ eau.addEventListener("click", () =>{
     message.textContent = playGame(player,pc);
 })
 terre.addEventListener("click", () =>{
-    let player = userChoice('Terre');
+    let player = userChoice("\u{1F331}");
     let pc = computerChoice();
 
     userResult.textContent = player;
     computerResult.textContent = pc;
     message.textContent = playGame(player,pc);
 })
-
 function playGame(userChoice, computerChoice){
     if (userChoice === computerChoice){
         return 'Egalité';
     }
-    else if (userChoice === "Feu" && computerChoice === "Terre"){
+    else if (userChoice === "\u{1F525}" && computerChoice === "\u{1F331}"){
         uScore++;
         return 'Bravo ! Tu as gagné!';
     }
-    else if (userChoice === "Eau" && computerChoice === "Feu"){
+    else if (userChoice === "\u{1F4A7}" && computerChoice === "\u{1F525}"){
         uScore++;
         return 'Bravo ! Tu as gagné!';
     }
-    else if (userChoice === "Terre" && computerChoice === "Eau"){
+    else if (userChoice === "\u{1F331}" && computerChoice === "\u{1F4A7}"){
         uScore++;
          return 'Bravo ! Tu as gagné!';
     }
@@ -68,4 +67,3 @@ function playGame(userChoice, computerChoice){
         return 'PERDU BOLOSS';
     }
 }
-
