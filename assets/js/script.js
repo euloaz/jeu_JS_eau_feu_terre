a6e6b8e133df03a4d0dccec65676c4d8ffaaa5c9
Héloïse <heloise.marie-oem@outlook.fr>
2025-10-15 12:16:24 +0200

    functions game ok

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,42 @@
-console.log('hello world');
+const choices = ["Feu","Eau","Terre"]
+function computerChoice(){
+    let random = Math.floor(Math.random()* choices.length);
+    return choices[random];
+}
+
+const feu = document.querySelector('#feu');
+const eau = document.querySelector('#eau');
+const terre = document.querySelector('#terre');
+
+function userChoice(choice){
+    return choice;
+}
+feu.addEventListener("click", () => {
+    playGame(userChoice('Feu'),computerChoice())
+})
+eau.addEventListener("click", () =>{
+   playGame(userChoice('Eau'),computerChoice())
+})
+terre.addEventListener("click", () =>{
+    playGame(userChoice('Terre'),computerChoice())
+})
+
+function playGame(userChoice, computerChoice){
+    if (userChoice === computerChoice){
+        return 'Egalité';
+    }
+    else if (userChoice === "Feu" && computerChoice === "Terre"){
+        return 'Bravo ! Tu as gagné!';
+    }
+    else if (userChoice === "Eau" && computerChoice === "Feu"){
+        return 'Bravo ! Tu as gagné!';
+    }
+    else if (userChoice === "Terre" && computerChoice === "Eau"){
+         return 'Bravo ! Tu as gagné!';
+    }
+    else{
+        return 'PERDU BOLOSS';
+    }
+
+}
+
